4093d3777d851e4fa33cdbdefebde15c32609cc8
Nguyen Nhat Truong <nhattruongnguyenag@gmail.com>
2023-06-03 01:26:10 +0700

    fix database

--- movies-project/app/Models/GenresModel.php
+++ movies-project/app/Models/GenresModel.php
@@ -34,7 +34,7 @@ class GenresModel extends Model
             $genres = new GenresModel();
         }
 
-        $genres->name = $requestObject->name;
+        $genres->title = $requestObject->name;
         $genres->save();
 
         return response($genres, 201);
@@ -51,6 +51,6 @@ class GenresModel extends Model
 
     function movies()
     {
-        return $this->belongsToMany(MovieModel::class, "genreses_movies", "genres_id", "movie_id");
+        return $this->belongsToMany(MovieModel::class, "genreses_movies", "genres_id", "move_id");
     }
 }
--- movies-project/resources/views/admin/genreses/genreses-list.blade.php
+++ movies-project/resources/views/admin/genreses/genreses-list.blade.php
@@ -1,74 +1,74 @@
 @extends('admin.admin')
 @section('content')
-    @php
-        $APIRoute = 'api-genreses';
-        $WebRoute = 'genreses';
-    @endphp
-    <div class="main-content">
-        <div class="main-content-inner">
-            @include('admin.components.breadcrumb')
-            <div class="page-content">
-                <div class="row">
-                    <div class="col-xs-12">
-                        <!-- PAGE CONTENT BEGINS -->
-                        <div class="row">
-                            <div class="col-xs-12">
-                                @include('admin.components.button-add-delete-group')
-                                <table id="simple-table" class="table table-striped table-bordered table-hover">
-                                    <thead>
-                                        <tr>
-                                            <th style="width: 5rem"></th>
-                                            <th class="center">Tên thể loại</th>
-                                            <th class="center">Thao tác</th>
-                                        </tr>
-                                    </thead>
+@php
+$APIRoute = 'api-genreses';
+$WebRoute = 'genreses';
+@endphp
+<div class="main-content">
+    <div class="main-content-inner">
+        @include('admin.components.breadcrumb')
+        <div class="page-content">
+            <div class="row">
+                <div class="col-xs-12">
+                    <!-- PAGE CONTENT BEGINS -->
+                    <div class="row">
+                        <div class="col-xs-12">
+                            @include('admin.components.button-add-delete-group')
+                            <table id="simple-table" class="table table-striped table-bordered table-hover">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 5rem"></th>
+                                        <th class="center">Tên thể loại</th>
+                                        <th class="center">Thao tác</th>
+                                    </tr>
+                                </thead>
 
-                                    <tbody>
-                                        @foreach ($genreses as $genres)
-                                            <tr>
-                                                <td class="center">
-                                                    <label class="pos-rel">
-                                                        <input type="checkbox" value="{{$genres->id}}" class="ace ids" />
-                                                        <span class="lbl"></span>
-                                                    </label>
-                                                </td>
-                                                <td class="center">{{ $genres->name }}</td>
-                                                <td class="center">
-                                                    <div class="hidden-sm hidden-xs btn-group">
-                                                        @php
-                                                            $editURI = 'admin/genreses/' . $genres->id . '/edit';
-                                                        @endphp
-                                                        <a href="{{ url($editURI) }}" class="btn btn-xs btn-info">
-                                                            <i class="ace-icon fa fa-pencil bigger-120"></i>
-                                                        </a>
+                                <tbody>
+                                    @foreach ($genreses as $genres)
+                                    <tr>
+                                        <td class="center">
+                                            <label class="pos-rel">
+                                                <input type="checkbox" value="{{$genres->id}}" class="ace ids" />
+                                                <span class="lbl"></span>
+                                            </label>
+                                        </td>
+                                        <td class="center">{{ $genres->title }}</td>
+                                        <td class="center">
+                                            <div class="hidden-sm hidden-xs btn-group">
+                                                @php
+                                                $editURI = 'admin/genreses/' . $genres->id . '/edit';
+                                                @endphp
+                                                <a href="{{ url($editURI) }}" class="btn btn-xs btn-info">
+                                                    <i class="ace-icon fa fa-pencil bigger-120"></i>
+                                                </a>
 
-                                                        <button data="{{ $genres->id }}" id="btnDelete{{ $genres->id }}"
-                                                            class="btn btn-xs btn-danger btn-delete">
-                                                            <i class="ace-icon fa fa-trash-o bigger-120"></i>
-                                                        </button>
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
+                                                <button data="{{ $genres->id }}" id="btnDelete{{ $genres->id }}"
+                                                    class="btn btn-xs btn-danger btn-delete">
+                                                    <i class="ace-icon fa fa-trash-o bigger-120"></i>
+                                                </button>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
 
-                                <div class="center">
-                                    {{ $genreses->links() }}
-                                </div>
-                            </div><!-- /.span -->
-                        </div><!-- /.row -->
-                        <div class="hr hr-18 dotted hr-double"></div>
-                    </div><!-- /.col -->
-                </div><!-- /.row -->
-            </div><!-- /.page-content -->
-        </div>
+                            <div class="center">
+                                {{ $genreses->links() }}
+                            </div>
+                        </div><!-- /.span -->
+                    </div><!-- /.row -->
+                    <div class="hr hr-18 dotted hr-double"></div>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.page-content -->
     </div>
-    <script src="{{ asset('admin/assets/js/crud-utils.js') }}"></script>
-    <script src="{{ asset('admin/assets/js/deleteWidget.js') }}"></script>
-    <script>
-        let apiURL = "{{ route($APIRoute) }}"
+</div>
+<script src="{{ asset('admin/assets/js/crud-utils.js') }}"></script>
+<script src="{{ asset('admin/assets/js/deleteWidget.js') }}"></script>
+<script>
+    let apiURL = "{{ route($APIRoute) }}"
         let responseURL = "{{ route($WebRoute) }}"
         deleleRequest(apiURL, responseURL)
-    </script>
+</script>
 @endsection
